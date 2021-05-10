2893e12052d8a9c5d6d73bf051b476b157b71fcd
Yacine Merzouk <yacine@merzouk.ca>
2021-05-09 16:25:38 -1000

    Updated version and constraints

--- package.js
+++ package.js
@@ -1,24 +1,24 @@
 Package.describe({
   name: 'meteorhubdotnet:firestorm-iron-dynamic-template',
   summary: 'Fork of iron:dynamic-template for Firestorm',
-  version: '1.0.0',
+  version: '1.0.4',
   git: 'https://github.com/meteorhubdotnet/iron-dynamic-template'
 });
 
 Package.on_use(function (api) {
   api.versionsFrom('METEOR@2.2');
 
-  api.use('blaze');
+  api.use('blaze@2.4.0');
   api.use('underscore');
-  api.use('ui');
-  api.use('jquery');
+  api.use('ui@1.0.13');
+  api.use('jquery@3.0.0');
   api.use('tracker');
   api.use('reactive-var');
-  api.use('templating');
+  api.use('templating@1.4.0');
   api.use('random');
 
-  api.use('meteorhubdotnet:firestorm-iron-core');
-  api.imply('meteorhubdotnet:firestorm-iron-core');
+  api.use('meteorhubdotnet:firestorm-iron-core@1.0.4');
+  api.imply('meteorhubdotnet:firestorm-iron-core@1.0.4');
 
   // api.add_files('version_conflict_error.js');
   api.add_files('dynamic_template.html');
